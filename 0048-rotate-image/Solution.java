78a82bc07fee0423cd4a8ba686d8f83bc0278f4c
Muaaz khan <khanmuaaz19204@gmail.com>
2026-08-26 00:35:09 +0530

    Implement image rotation in Solution.java

---
+++ 0048-rotate-image/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] arr) {
+         //Transpose
+        for(int i=1;i<arr.length;i++){
+            for(int j=0;j<i;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        //Reverse Each row
+        for(int i=0;i<arr.length;i++){
+            int stCol=0, endCol=arr[0].length-1;
+            while(stCol<endCol){
+                int temp = arr[i][stCol];
+                arr[i][stCol]=arr[i][endCol];
+                arr[i][endCol]=temp;
+                stCol++;
+                endCol--;
+                
+            }
+        }
+        
+    }
+}
